e34da4e8f11b24de144d08c2a89cb72408df9584
notmvson <mason5kong@yahoo.com>
2024-01-11 18:05:01 -0800

    created loop w prompts, an arry, and the html section that displays the words (.html and .js)

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+function enterWords() {
+    let wordsArray = []; //array
+
+    for (var i=0; i < 3; i++) {
+        let word = prompt('Enter word ' + (1+i) + ': ');
+
+        if (word == null || word == "") {
+            alert('Please enter a valid word');
+            i--;
+        }
+        else {
+            wordsArray.push(word);
+        }
+    }
+    document.getElementById('first').innerText = wordsArray[0];
+    document.getElementById('second').innerText = wordsArray[1];
+    document.getElementById('third').innerText = wordsArray[2];
+}
+enterWords();
+
 document.addEventListener("DOMContentLoaded", function(event) {
     
 })
