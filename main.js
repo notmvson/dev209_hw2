427b731ae42659883d3e7c94920f50e851512d0c
notmvson <mason5kong@yahoo.com>
2024-01-11 20:37:33 -0800

    added second html section, fixed button, defined button event listener (not button function yet), and added notes in js for clarity

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
-function enterWords() {
-    let wordsArray = []; //array
+let swapButton = document.getElementById('swap');
 
-    for (var i=0; i < 3; i++) {
-        let word = prompt('Enter word ' + (1+i) + ': ');
 
-        if (word == null || word == "") {
-            alert('Please enter a valid word');
-            i--;
+
+document.addEventListener("DOMContentLoaded", function(event) {    
+    function enterWords() { // loop that asks for 3 words
+        let wordsArray = []; //array
+
+        for (var i=0; i < 3; i++) {
+            let word = prompt('Enter word ' + (1+i) + ': ');
+
+            if (word == null || word == "") {
+                alert('Please enter a valid word');
+                i--;
+            }
+            else {
+                wordsArray.push(word);
+            }
         }
-        else {
-            wordsArray.push(word);
-        }
+        // lists the three words
+        document.getElementById('first').innerText = wordsArray[0];
+        document.getElementById('second').innerText = wordsArray[1];
+        document.getElementById('third').innerText = wordsArray[2];
+
+        swapButton.style.display = 'block'; // only visible when 3 words displayed
     }
-    document.getElementById('first').innerText = wordsArray[0];
-    document.getElementById('second').innerText = wordsArray[1];
-    document.getElementById('third').innerText = wordsArray[2];
-}
-enterWords();
+    enterWords();
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    
+    swapButton.addEventListener("click", swapLetters, false); //define button event when clicked   
 })
