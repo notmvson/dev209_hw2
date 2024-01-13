328fb9471c3f9e59c07ee31d4d10c71b79f228d3
Gunnyyyy <135295777+Gunnyyyy@users.noreply.github.com>
2024-01-13 11:07:33 -0800

    Update main.js
    Variables swapButton, preSwapSection, and swappedSection are declared

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-let swapButton = document.getElementById('swap');
-
-swapped.style.display = 'none'; // second section hidden
-
 document.addEventListener("DOMContentLoaded", function(event) {    
-    function enterWords() { // loop that asks for 3 words
-        let wordsArray = []; //array
+    // Variables for button and sections
+    let swapButton = document.getElementById('swap');
+    let preSwapSection = document.getElementById('preSwap');
+    let swappedSection = document.getElementById('swapped');
 
+    swappedSection.style.display = 'none'; // second section hidden
+    
+    let wordsArray = []; //array
+
+    function enterWords() { // loop that asks for 3 words
+        
+        wordsArray = [];
+        
         for (var i=0; i < 3; i++) {
             let word = prompt('Enter word ' + (1+i) + ': ');
 
-            if (word == null || word == "") {
+            if (word == null || word.trim() === "") {
                 alert('Please enter a valid word');
                 i--;
             }
@@ -17,6 +23,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 wordsArray.push(word);
             }
         }
+        
         // lists the three words
         document.getElementById('first').innerText = wordsArray[0];
         document.getElementById('second').innerText = wordsArray[1];
@@ -30,7 +37,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
     swapButton.addEventListener("click", swapLetters, false); //defined button event when clicked   
     
     function swapLetters () { // hides first section and shows second section
-        preSwap.style.display = "none";
-        swapped.style.display = "block";
+        preSwapSection.style.display = "none";
+        swappedSection.style.display = "block";
+
+        // Creates new array with swapped first and last letters
+        let swappedArray = wordsArray.map(function(oneWord) {
+            return oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length -1) + oneWord.charAt(0);
+        });
+
+        // Display swapped words in second section
+        document.getElementById('swappedFirst').innerText = swappedArray[0];
+        document.getElementById('swappedSecond').innerText = swappedArray[1];
+        document.getElementById('swappedThird').innerText = swappedArray[2];
     }
-})
+});
